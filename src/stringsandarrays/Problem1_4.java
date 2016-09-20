f3f94359d6692ee660666cebbf35d0995a846657
chenjihan <chenjihan1022@hotmail.com>
2016-09-19 21:19:53 -0400

    Initial Commit

---
+++ src/stringsandarrays/Problem1_4.java
@@ -0,0 +1,30 @@
+package stringsandarrays;
+
+public class Problem1_4 {
+
+	public static void main(String[] args) {
+		System.err.println(new Problem1_4().replaceSpaces(" "));
+	}
+
+	public String replaceSpaces(String s) {
+		if (s == null) return null;
+		int count = 0;
+		char[] charArr = s.toCharArray();
+		for (int i = 0; i < charArr.length; i++) {
+			if (charArr[i] == ' ') count++;
+		}
+		char[] newCharArr = new char[charArr.length + count * 2];
+		int j = 0;
+		for (int i = 0; i < charArr.length; i++) {
+			if (charArr[i] == ' ') {
+				newCharArr[j++] = '%';
+				newCharArr[j++] = '2';
+				newCharArr[j++] = '0';
+			} else {
+				newCharArr[j++] = charArr[i];
+			}
+		}
+		return String.valueOf(newCharArr);
+	}
+
+}
---
+++ src/utils/P.java
@@ -0,0 +1,16 @@
+package utils;
+
+public class P {
+
+	public static void println(Object obj) {
+		System.out.println(obj);
+	}
+
+	public static void print(Object obj) {
+		System.out.println(obj);
+	}
+
+	public static void println() {
+		System.out.println();
+	}
+}
