9b58b8fbb4daa5db30b75c876a4eab85d0aab09d
chenjihan <chenjihan1022@hotmail.com>
2016-09-23 15:56:36 -0400

    Problem2_7

--- src/main/java/linkedlist/Problem2_3.java
+++ src/main/java/linkedlist/Problem2_3.java
@@ -4,28 +4,29 @@ import utils.TestData;
 import utils.Z;
 
 /**
- * Since it's decribed like "In the middle of a singly linked list", so the given should not be the
- * last one. Then some edge case can be omitted.
+ * Since it's decribed like "In the middle of a singly linked list", so the
+ * given should not be the last one. Then some edge case can be omitted.
  *
  * @author Jihan Chen
  *
  */
 public class Problem2_3 {
 
-  public static void main(String[] args) {
-    ListNode head = new TestData().head;
-    ListNode node = new TestData().node5;
-    Z.printLinkedList(head);
-    Z.println();
-    Z.printLinkedList(node);
-    Z.println();
-    new Problem2_3().removeSpecifiedNode(node);
-    Z.printLinkedList(head);
-  }
+	public static void main(String[] args) {
+		TestData td = new TestData();
+		ListNode head = td.head;
+		ListNode node = td.node5;
+		Z.printLinkedList(head);
+		Z.println();
+		Z.printLinkedList(node);
+		Z.println();
+		new Problem2_3().removeSpecifiedNode(node);
+		Z.printLinkedList(head);
+	}
 
-  public void removeSpecifiedNode(ListNode node) {
-    node.val = node.next.val;
-    node.next = node.next.next;
-  }
+	public void removeSpecifiedNode(ListNode node) {
+		node.val = node.next.val;
+		node.next = node.next.next;
+	}
 
 }
