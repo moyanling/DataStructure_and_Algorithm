c5562181561a725da9deacbda6984e2ecb5e95d3
Jihan Chen <chen39@umd.edu>
2016-11-01 13:02:05 -0400

    Longest Common Sub-sequence
    * Recursive solution : Implementation and test case.

--- src/main/java/org/mo39/fmbh/algorithm/dynamicprogramming/LongestCommonSubsequence.java
+++ src/main/java/org/mo39/fmbh/algorithm/dynamicprogramming/LongestCommonSubsequence.java
@@ -1,11 +1,13 @@
 package org.mo39.fmbh.algorithm.dynamicprogramming;
 
+import static org.hamcrest.CoreMatchers.hasItems;
+
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.ArrayUtils;
+import org.junit.Assert;
 import org.junit.Test;
-import org.mo39.fmbh.common.Z;
 
 /**
  * Given two string, find the longest common sub-sequence.
@@ -15,46 +17,40 @@ import org.mo39.fmbh.common.Z;
  */
 public enum LongestCommonSubsequence {
 
-  SOLUTION() {
+  RECURSIVE_SOLUTION() {
 
     @Override
-    public List<List<Character>> solve(String s1, String s2) {
-      List<CharList> pre = new ArrayList<>();
-      List<CharList> curr = new ArrayList<>();
-
-      for (int len = 2; len < s1.length() + 1; len++) {
-        for (CharList cList : pre) {
-
-        }
-      }
-      return toList(curr);
+    public char[] solve(String s1, String s2) {
+      return recur(s1.toCharArray(), s1.length(), s2.toCharArray(), s2.length());
     }
 
-    private List<List<Character>> toList(List<CharList> cList) {
-      return cList.stream().map(o -> o.cList).collect(Collectors.toList());
+    private char[] recur(char[] charArr1, int end1, char[] charArr2, int end2) {
+      if (end1 == 0 || end2 == 0) return new char[0];
+      if (charArr1[end1 - 1] == charArr2[end2 - 1])
+        return ArrayUtils.add(recur(charArr1, end1 - 1, charArr2, end2 - 1), charArr1[end1 - 1]);
+      char[] result1 = recur(charArr1, end1 - 1, charArr2, end2);
+      char[] result2 = recur(charArr1, end1, charArr2, end2 - 1);
+      return result1.length > result2.length ? result1 : result2;
     }
 
   };
 
-  public abstract List<List<Character>> solve(String s1, String s2);
+  public abstract char[] solve(String s1, String s2);
 
-  private class CharList {
-
-    public List<Character> cList;
-    public int lastIndex;
-
-    public CharList() {
-      cList = new ArrayList<>();
-    }
-
-  }
-
+  @SuppressWarnings("serial")
   public static class TestLongestCommonSubsequence {
 
+    private List<char[]> expected = new ArrayList<char[]>() {
+      {
+        add(new char[] {'B', 'D', 'A', 'B'});
+        add(new char[] {'B', 'C', 'A', 'B'});
+      }
+    };
+
     @Test
-    public void testSolution() {
-      Z.print(LongestCommonSubsequence.SOLUTION.solve("ABCBDAB", "BDCABA"));
-      Z.print(String.valueOf(new char[] {'C', 'B'}));
+    public void testRecursiveSolution() {
+      Assert.assertThat(expected,
+          hasItems(LongestCommonSubsequence.RECURSIVE_SOLUTION.solve("ABCBDAB", "BDCABA")));
     }
 
   }
