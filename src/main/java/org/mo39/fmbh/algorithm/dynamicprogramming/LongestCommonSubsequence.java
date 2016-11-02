bc25a54e212fc4fd56c9e8abd640b546ad6eaa7e
Jihan Chen <chen39@umd.edu>
2016-11-02 12:15:43 -0400

    Longest Common sub-sequence
    * Added comments and recurrence formula for recursive solution

--- src/main/java/org/mo39/fmbh/algorithm/dynamicprogramming/LongestCommonSubsequence.java
+++ src/main/java/org/mo39/fmbh/algorithm/dynamicprogramming/LongestCommonSubsequence.java
@@ -17,6 +17,29 @@ import org.junit.Test;
  */
 public enum LongestCommonSubsequence {
 
+  /**
+   * After learning this solution, I got the feeling that for every recursive problem/solution, I
+   * should provide a <b>Recurrence Formula</b> to show<br>
+   * 1. how a problem can solve it's previous cases using the same solution and, <br>
+   * 2. the stop case of the recurrence.<br>
+   * <p>
+   * It is clear that, given this formula/function, I can implement the code easily with a good
+   * understanding of the problem. And, a implementation without such formula may allow me to
+   * swallow some wrong understanding.
+   * <p>
+   * Recurrence Formula:<br>
+   *
+   * <pre>
+   * Given X = &lt;x[1], x[2], ..., x[m]&gt;
+   * for i = 0 , 1, ..., m,
+   * Define X[i] = &lt;x[1], x[2], ..., x[i]&gt; is the i<sup>th</sup> prefix of X.
+   * And define c[i,j] is the length of the LCS of X[i] and Y[j].
+   *
+   *          | 0                           if i=0 or j=0
+   * c[i,j] = | c[i-1,j-1] + 1              if i,j>0 and x[i] = y[j]
+   *          | max(c[i,j-1], c[i-1,j])     if i,j>0 and x[i] != y[j]
+   * </pre>
+   */
   RECURSIVE_SOLUTION() {
 
     @Override
@@ -33,6 +56,16 @@ public enum LongestCommonSubsequence {
       return result1.length > result2.length ? result1 : result2;
     }
 
+  },
+
+  ITERATIVE_SOLUTION() {
+
+    @Override
+    public char[] solve(String s1, String s2) {
+      // TODO Auto-generated method stub
+      return null;
+    }
+
   };
 
   public abstract char[] solve(String s1, String s2);
@@ -40,7 +73,7 @@ public enum LongestCommonSubsequence {
   @SuppressWarnings("serial")
   public static class TestLongestCommonSubsequence {
 
-    private List<char[]> expected = new ArrayList<char[]>() {
+    private List<char[]> list = new ArrayList<char[]>() {
       {
         add(new char[] {'B', 'D', 'A', 'B'});
         add(new char[] {'B', 'C', 'A', 'B'});
@@ -49,7 +82,7 @@ public enum LongestCommonSubsequence {
 
     @Test
     public void testRecursiveSolution() {
-      Assert.assertThat(expected,
+      Assert.assertThat(list,
           hasItems(LongestCommonSubsequence.RECURSIVE_SOLUTION.solve("ABCBDAB", "BDCABA")));
     }
 
