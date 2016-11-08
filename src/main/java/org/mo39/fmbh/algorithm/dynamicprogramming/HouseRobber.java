495711aab4216a8a719e8051def0e8cf9d2c23de
Jihan Chen <chen39@umd.edu>
2016-11-08 11:59:30 -0500

    House Robber
    * Bottom-up method 0, 1 and 2

--- src/main/java/org/mo39/fmbh/algorithm/dynamicprogramming/HouseRobber.java
+++ src/main/java/org/mo39/fmbh/algorithm/dynamicprogramming/HouseRobber.java
@@ -4,7 +4,6 @@ import static org.mo39.fmbh.common.annotation.ProblemSource.SourceValue.LEETCODE
 
 import org.junit.Assert;
 import org.junit.Test;
-import org.mo39.fmbh.common.Z;
 import org.mo39.fmbh.common.annotation.ProblemSource;
 
 
@@ -63,8 +62,7 @@ public enum HouseRobber {
       this.nums = nums;
       memo = new Integer[nums.length];
       int result = recur(nums.length - 1);
-      Z.print(memo);
-       return result;
+      return result;
     }
 
     public int recur(int i) {
@@ -78,23 +76,87 @@ public enum HouseRobber {
 
   },
 
-  BOTTOM_UP_METHOD {
-
-    private int[] memo;
+  /**
+   * dp[i][1] means rob the current house and dp[i][0] means don't.
+   */
+  BOTTOM_UP_METHOD_0 {
 
     @Override
     public int solve(int[] nums) {
-      if (nums.length == 1) return nums[0];
-      if (nums.length == 2) return Math.max(nums[0], nums[1]);
-      memo = new int[nums.length];
-      for (int i = 0; i < nums.length; i++) {
-        if (i == 0) memo[0] = nums[0];
-
+      int[][] dp = new int[nums.length + 1][2];
+      for (int i = 1; i <= nums.length; i++) {
+        dp[i][0] = Math.max(dp[i - 1][0], dp[i - 1][1]);
+        dp[i][1] = nums[i - 1] + dp[i - 1][0];
       }
-
-      // TODO Auto-generated method stub
-      return 0;
+      return Math.max(dp[nums.length][0], dp[nums.length][1]);
     }
+
+  },
+
+
+  /**
+   * The solution of this problem yields the fact that, the current solution only depends on one or
+   * two previous solutions. So the scale is //TODO. That's why this can be solved with <b>O(1)</b>
+   * space. The time complexity is <b>O(n)</b>;
+   *
+   */
+  BOTTOM_UP_METHOD_1 {
+
+    /**
+     * This variable stores the sum of the previous houses and can add nums[i] because it does not
+     * have nums[i-1] in the sum;
+     */
+    private int toAdd = 0;
+
+    /**
+     * This variable stores the sum of the previous houses and can not add nums[i] because it has
+     * nums[i-1] in the sum;
+     */
+    private int cantAdd = 0;
+
+    @Override
+    public int solve(int[] nums) {
+      for (int n : nums) {
+        toAdd += n;
+        if (toAdd < cantAdd) {
+          /**
+           * If we rob current but total money is still smaller than the one not rob, then why
+           * matter to rob it? This makes us lose the chance to rob next house.
+           */
+          toAdd = cantAdd;
+        } else {
+          /**
+           * Otherwise, since we add the current house to toAdd, it can not add next house next
+           * time, so swap it with cantAdd.
+           */
+          int temp = toAdd;
+          toAdd = cantAdd;
+          cantAdd = temp;
+        }
+      }
+      return Math.max(toAdd, cantAdd);
+    }
+
+  },
+
+  /**
+   * This one use almost the same idea but a better implementation compared to
+   * {@link BOTTOM_UP_METHOD_1}
+   */
+  BOTTOM_UP_METHOD_2 {
+
+    @Override
+    public int solve(int[] nums) {
+      int prevNo = 0;
+      int prevYes = 0;
+      for (int n : nums) {
+        int temp = prevNo;
+        prevNo = Math.max(prevNo, prevYes);
+        prevYes = n + temp;
+      }
+      return Math.max(prevNo, prevYes);
+    }
+
 
   };
 
@@ -111,8 +173,15 @@ public enum HouseRobber {
     }
 
     @Test
-    public void testBottomUpWithMemo() {
+    public void testTopDownWithMemo() {
       Assert.assertEquals(expected, HouseRobber.TOP_DOWN_WITH_MEMO.solve(nums));
+    }
+
+    @Test
+    public void testBottomUpMethod() {
+      Assert.assertEquals(expected, HouseRobber.BOTTOM_UP_METHOD_0.solve(nums));
+      Assert.assertEquals(expected, HouseRobber.BOTTOM_UP_METHOD_1.solve(nums));
+      Assert.assertEquals(expected, HouseRobber.BOTTOM_UP_METHOD_2.solve(nums));
     }
 
   }
